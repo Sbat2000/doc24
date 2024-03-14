385958052f93bbd59f139d3b38df3e5c0699302c
Sbat2000 <zevzok@gmail.com>
2024-03-14 19:49:04 +0800

    Initial Commit

---
+++ Doc/DocApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocApp.swift
+//  Doc
+//
+//  Created by Aleksandr Garipov on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
